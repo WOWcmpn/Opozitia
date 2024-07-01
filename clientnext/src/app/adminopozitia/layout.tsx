3a41027daae103d55370a46dae1c18a32018a020
OtherOl <pilyak003@gmail.com>
2024-07-01 22:51:29 +0300

    fix vercel

--- clientnext/src/app/adminopozitia/layout.tsx
+++ clientnext/src/app/adminopozitia/layout.tsx
@@ -1,7 +1,4 @@
-import { Inter } from 'next/font/google';
 import { Metadata } from 'next';
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Opozitia-adminopozitia",
@@ -12,10 +9,8 @@ export default function RootLayout({children}: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-    <body className={inter.className}>
+    <div>
       {children}
-    </body>
-    </html>
+    </div>
   );
 };
