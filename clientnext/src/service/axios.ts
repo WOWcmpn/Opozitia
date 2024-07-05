432c85012b03d8c2d7052e13c74a45234fc22219
OtherOl <pilyak003@gmail.com>
2024-07-05 18:50:10 +0300

    refactoring

--- clientnext/src/service/axios.ts
+++ clientnext/src/service/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:4000",
-  // baseURL: "https://opozitia-server.vercel.app",
+  //baseURL: "http://localhost:4000",
+  baseURL: "https://opozitia-server.vercel.app",
   // baseURL: "https://superm0m.beget.tech",
 });
